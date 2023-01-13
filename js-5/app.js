714eacc9fe32cc6787ebbbeed324ff55a7fd7cea
Israfilli Ramin <106513116+IsrafilliRamin@users.noreply.github.com>
2023-01-13 09:56:59 +0400

    Add files via upload

---
+++ js-5/app.js
@@ -0,0 +1,80 @@
+
+/*  Array find metodu */
+
+
+    let result = [1,2,3,4,5];
+
+/* 
+    let filter = result.filter((item)=>item>1)
+
+    console.log(filter);
+
+    let result1 = [1,2,3,4,5];
+    let find = result1.find((item)=>item > 1);
+
+    console.log(find); */
+
+    /* Array Reducer */
+
+
+/*     let reducer = result.reduce((acc,item)=>acc+item);
+    console.log(reducer);
+
+
+
+    let meyveler = [
+        {
+            name:"alma",
+            salery: 150,
+        },
+        {
+            name:"banan",
+            salery: 250,
+        },
+        {
+            name:"armud",
+            salery: 350,
+        }
+    ]
+
+
+
+        let newMeyve = meyveler.reduce((acc,item)=>acc+item.salery,0)
+            console.log(newMeyve); */
+
+
+
+    /* Array Reverse */
+
+   /*  let students = ["Ramin","Murad","Xakizer","Ulker"];
+    let numbers = [1,2,3,4,5];
+    console.log(numbers.reverse());
+    console.log(students.reverse());
+
+   
+
+
+     let name2 = "reklU";
+
+     console.log(name2.split("").reverse().join("")); */
+
+
+     /*  Array sort metodu */
+
+    /*     let num =  [2,3,4,5,1];
+    let students = ["Ramin","Murad","Xakizer","Ulker"];
+      console.log(students.sort());
+        console.log(num.sort((a,b)=>b-a)) */
+
+
+
+
+
+
+    
+
+
+
+
+
+
